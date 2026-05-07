d38e969794f4dcfdb6b6e32e8f7b62b172228bdc
Melech Mizrachi <melechmizrachi@gmail.com>
2026-05-07 09:37:57 -0700

    [Http] Add http stubs.

---
+++ src/main/java/io/valkyrja/http/message/request/factory/RequestFactory.java
@@ -0,0 +1,19 @@
+/*
+ * This file is part of the Valkyrja Framework package.
+ *
+ * (c) Melech Mizrachi <melechmizrachi@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+package io.valkyrja.http.message.request.factory;
+
+import io.valkyrja.http.message.request.contract.ServerRequestContract;
+
+public class RequestFactory {
+
+    public static ServerRequestContract fromGlobals() {
+        return null;
+    }
+}
