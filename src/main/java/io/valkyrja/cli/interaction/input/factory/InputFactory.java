a8428727a2fe413c27d1c064ff1958187f83d1b2
Melech Mizrachi <melechmizrachi@gmail.com>
2026-05-07 09:38:03 -0700

    [Cli] Add cli stubs.

---
+++ src/main/java/io/valkyrja/cli/interaction/input/factory/InputFactory.java
@@ -0,0 +1,20 @@
+/*
+ * This file is part of the Valkyrja Framework package.
+ *
+ * (c) Melech Mizrachi <melechmizrachi@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+package io.valkyrja.cli.interaction.input.factory;
+
+import io.valkyrja.cli.interaction.input.contract.InputContract;
+
+public class InputFactory {
+
+    public static InputContract fromGlobals(
+            String[] args, String applicationName, String defaultCommandName) {
+        return null;
+    }
+}
