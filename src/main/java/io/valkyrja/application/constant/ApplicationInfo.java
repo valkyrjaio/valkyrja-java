4a7f658759a6e2342dfa30b3c5ec7df4720872dd
MelechMizrachi <1179171+MelechMizrachi@users.noreply.github.com>
2026-07-25 08:15:14 +0000

    [Release] Update ApplicationInfo for v26.2.0 release

--- src/main/java/io/valkyrja/application/constant/ApplicationInfo.java
+++ src/main/java/io/valkyrja/application/constant/ApplicationInfo.java
@@ -13,9 +13,9 @@ public class ApplicationInfo {
 
     private ApplicationInfo() {}
 
-    public static final String VERSION = "26.1.2";
+    public static final String VERSION = "26.2.0";
 
-    public static final String VERSION_BUILD_DATE_TIME = "June 19 2026 23:48:54 MST";
+    public static final String VERSION_BUILD_DATE_TIME = "July 25 2026 01:13:37 MST";
 
     public static final String ASCII =
             """
