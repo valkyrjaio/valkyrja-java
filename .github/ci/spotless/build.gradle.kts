1956f5a17388828d00fcaa3cf39ae69e1d418de8
Melech Mizrachi <melechmizrachi@gmail.com>
2026-08-02 20:43:16 -0700

    [Spotless] refactor: Take the copyright header from the shared package.

--- .github/ci/spotless/build.gradle.kts
+++ .github/ci/spotless/build.gradle.kts
@@ -6,6 +6,18 @@
  * Released under the MIT License. See LICENSE.md for details.
  */
 
+import io.valkyrja.spotless.CopyrightHeader
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("io.valkyrja:ci-spotless-java:26.0.0")
+    }
+}
+
 plugins {
     id("com.diffplug.spotless") version "8.9.0"
     id("com.github.ben-manes.versions") version "0.58.0"
@@ -44,16 +56,6 @@ spotless {
             ".github/ci/archunit/src/test/java/**/*.java",
         )
         googleJavaFormat("1.27.0").aosp()
-        licenseHeader(
-            """/*
- * This file is part of the Valkyrja Framework package.
- *
- * Copyright (c) 2016-present Melech Mizrachi
- *
- * Released under the MIT License. See LICENSE.md for details.
- */
-
-"""
-        )
+        licenseHeader(CopyrightHeader.block("Valkyrja Framework"))
     }
 }
